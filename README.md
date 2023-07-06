fb8cbeab91a806c3947c4035f28685a67b7fe44e
Haselnussbomber <mail@haselnussbomber.de>
2023-07-06 22:34:46 +0200

    resolve addons

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ just throw the dll into ```ReClass.Net\x64\Plugins\``` and set the path to ```FF
 * shows what class a pointer points at if available
 * adds a slightly improved C# Code Generator to generate strucs for FFXIVClientStructs
 * resolves instance names to addresses where available (e.g. `<Client::Game::Object::GameObjectManager_Instance>`)
+* resolves Addon addresses with `<Addon(id)>` or `<Addon(name)>`
 * resolves Agent addresses with `<Agent(id)>` or `<Agent(name)>` if the name is defined in data.yml
 * adds a live list of all agents that shows their ID, Size, defined Class name in data.yml and the name of the Addon that's currently using it
--- XivReClassPlugin/Patches/ModuleNamePatch.cs
+++ XivReClassPlugin/Patches/ModuleNamePatch.cs
@@ -4,6 +4,7 @@ using ReClassNET.Memory;
 using System.Text.RegularExpressions;
 using HarmonyLib;
 using XivReClassPlugin.Game;
+using System.Text;
 
 namespace XivReClassPlugin.Patches; 
 
@@ -11,6 +12,8 @@ namespace XivReClassPlugin.Patches;
 public class ModuleNamePatch {
 	private static readonly Regex AgentIdRegex = new("Agent\\((?<AgentId>\\d+)\\)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 	private static readonly Regex AgentNameRegex = new("Agent\\((?<AgentName>\\w+)\\)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+	private static readonly Regex AddonIdRegex = new("Addon\\((?<AddonId>\\d+)\\)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+	private static readonly Regex AddonNameRegex = new("Addon\\((?<AddonName>\\w+)\\)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
 	public static bool Prefix(ref Module __result, string name) {
 		if (string.IsNullOrEmpty(name))
@@ -47,6 +50,34 @@ public class ModuleNamePatch {
 			__result = new Module { Start = (nint)agent.Address, Name = agent.Name };
 			return false;
 		}
+
+		var matchAddonId = AddonIdRegex.Match(name);
+		var matchAddonName = AddonNameRegex.Match(name);
+		if ((matchAddonId.Success || matchAddonName.Success) && Ffxiv.Address.AtkUnitManager != 0) {
+			AtkUnitManager.Update();
+			Addon? addon = null;
+
+			if (matchAddonId.Success) {
+				if (!uint.TryParse(matchAddonId.Groups["AddonId"].Value, out var AddonId))
+					return true;
+
+				if (!AtkUnitManager.TryGetAddonById(AddonId, out addon))
+					return true;
+			}
+			else if (matchAddonName.Success) {
+				if (matchAddonName.Groups["AddonName"].Value is not { } AddonName)
+					return true;
+
+				if (!AtkUnitManager.TryGetAddonByName(AddonName, out addon))
+					return true;
+			}
+
+			if (addon != null) {
+				__result = new Module { Start = (nint)addon.Address, Name = addon.Name };
+				return false;
+			}
+		}
+
 		return true;
 	}
 }
